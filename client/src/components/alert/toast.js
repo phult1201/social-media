147fe6e9a1194e79c437c5af916141d76856e4f2
14110145 <waptop4u@gmail.com>
2021-06-03 20:37:19 +0700

    4.

--- client/src/components/alert/toast.js
+++ client/src/components/alert/toast.js
@@ -6,13 +6,12 @@ const Toast = ({ msg, handleShow, bgColor }) => {
       className={`toast show position-fixed text-light ${bgColor}`}
       style={{ top: "5px", right: "5px", minWidth: "200px", zIndex: 50 }}
     >
-      <div className={`toast-header text-light ${bgColor}`}>
+      <div className={`toast-header ${bgColor}`}>
         <strong className="mr-auto text-light">{msg.title}</strong>
         <button
           className="ml-2 mb-1 close"
           data-dismiss="toast"
           aria-label="Close"
-          style={{ outline: "none" }}
           onClick={handleShow}
         >
           &times;
